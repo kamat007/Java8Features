1f8eb54287a0720aedf405ab4011b6c295245e36
Rishi Kumar Kamat <rkkamat2001@gmail.com>
2022-05-13 17:19:23 +0530

    Added Multiply.java

---
+++ src/com/multiply/Multiply.java
@@ -0,0 +1,11 @@
+package com.multiply;
+
+public class Multiply {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b=10;
+		System.out.println("Multiplication is: " + (a*b));
+	}
+
+}
